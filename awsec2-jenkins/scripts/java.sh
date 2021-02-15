b94d9e7fdf72a4150e64cb29586987e66cf5a260
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-02-15 14:57:22 +0200

    modify path

--- awsec2-jenkins/scripts/java.sh
+++ awsec2-jenkins/scripts/java.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 sudo yum -y install java-1.8.0-openjdk-devel
+cd ~
+cat <<EOF | sudo tee .bash_profile
+# .bash_profile
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# User specific environment and startup programs
+
 JAVA_HOME=$(find /usr/lib/jvm/java-1.8* | head -n 3 | grep "/usr/lib/jvm/java-1.8.0-openjdk-1.8.0*")
-export JAVA_HOME
-PATH=$PATH:$JAVA_HOME
+PATH=$PATH:$HOME/bin:$JAVA_HOME
+
+export PATH
+EOF
