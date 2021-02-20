c1dbdf3d1afc8f9cac9fedbce3a0f6631f209a9a
manujakau <manujakau@gmail.com>
2021-02-20 14:06:08 +0200

    add jenkins host cdk setup

--- awsec2-jenkins-via-awscdk/awsec2_jenkins_via_awscdk/awsec2_jenkins_via_awscdk_stack.py
+++ awsec2-jenkins-via-awscdk/awsec2_jenkins_via_awscdk/awsec2_jenkins_via_awscdk_stack.py
@@ -1,4 +1,8 @@
-from aws_cdk import core
+from os import name
+from aws_cdk import (
+    core,
+    aws_ec2
+)
 
 
 class Awsec2JenkinsViaAwscdkStack(core.Stack):
@@ -7,3 +11,56 @@ class Awsec2JenkinsViaAwscdkStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        prod_config = self.node.try_get_context('envs')['prod']
+
+        custom_vpc = aws_ec2.Vpc(
+            self,
+            "DevopsVpcID",
+            name="DevOpsVPC",
+            cidr=prod_config['vpc_config']['vpc_cidr'],
+            max_azs=2,
+            nat_gateways=1,
+            subnet_configuration=[
+                aws_ec2.SubnetConfiguration(
+                    name="PublicSubnet", cidr_mask=prod_config['vpc_config']['cidr_mask'], subnet_type=aws_ec2.SubnetType.PUBLIC
+                ),
+                aws_ec2.SubnetConfiguration(
+                    name="PrivateSubnet", cidr_mask=prod_config['vpc_config']['cidr_mask'], subnet_type=aws_ec2.SubnetType.PRIVATE
+                ),
+                aws_ec2.SubnetConfiguration(
+                    name="DbSubnet", cidr_mask=prod_config['vpc_config']['cidr_mask'], subnet_type=aws_ec2.SubnetType.ISOLATED
+                )
+            ]
+        )
+
+        #simple tagging
+        core.Tags.of(custom_vpc).add("Owner", "Admin")
+
+        #import user-data scripts
+        with open("userdata_scripts/setup.sh", mode="r") as file:
+            user_data = file.read()
+
+        #ec2
+        test_server = aws_ec2.Instance(
+            self,
+            "ec2id",
+            instance_type=aws_ec2.InstanceType(instance_type_identifier="t2.micro"),
+            instance_name="Jenkins-Host",
+            machine_image=aws_ec2.MachineImage.generic_linux(ami_map=
+                {
+                    "eu-central-1": "ami-03c3a7e4263fd998c"
+                }
+            ),
+            vpc=custom_vpc,
+            vpc_subnets=aws_ec2.SubnetSelection(
+                subnet_type=aws_ec2.SubnetType.PUBLIC
+            ),
+            key_name="SAA-C01",
+            user_data=aws_ec2.UserData.custom(user_data)
+        )
+
+        #allow web traffic
+        test_server.connections.allow_from_any_ipv4(
+            aws_ec2.Port.tcp(80),
+            description="allow web traffic"
+        )
