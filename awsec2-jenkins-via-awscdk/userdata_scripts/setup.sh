c59468d8e49507f10a0ea5a231eaf4187c1a0ab4
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-02-23 13:50:28 +0200

    update scripts

--- awsec2-jenkins-via-awscdk/userdata_scripts/setup.sh
+++ awsec2-jenkins-via-awscdk/userdata_scripts/setup.sh
@@ -11,9 +11,8 @@ sudo chkconfig jenkins on
 
 sudo yum -y install git
 
-cd /opt
 sudo wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 sudo tar -xvzf apache-maven-3.6.3-bin.tar.gz
-sudo mv apache-maven-3.6.3 maven
+sudo mv apache-maven-3.6.3 maven && sudo mv maven /usr/local/
 
 sudo hostnamectl set-hostname jenkins
