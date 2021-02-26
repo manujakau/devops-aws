24223677e3f109d2c7267fc294986a86b611e522
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-02-26 11:37:22 +0200

    test

--- infra-via-awscdk/userdata_scripts/docker.sh
+++ infra-via-awscdk/userdata_scripts/docker.sh
@@ -4,5 +4,7 @@ sudo hostnamectl set-hostname docker
 sudo yum install docker -y
 sudo service docker start
 
-sudo useradd dockeradmin && sudo echo "dockeradmin:dockeradmin" | sudo chpasswd
+password="admin"
+sudo useradd dockeradmin
+yes $password | sudo passwd dockeradmin
 sudo usermod -aG docker dockeradmin
