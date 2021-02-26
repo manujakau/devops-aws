f5672c40cc071fb56516a17d8eb20ef52a3fe571
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2021-02-26 10:28:32 +0200

    update docker install script

--- infra-via-awscdk/userdata_scripts/docker.sh
+++ infra-via-awscdk/userdata_scripts/docker.sh
@@ -4,5 +4,5 @@ sudo hostnamectl set-hostname docker
 sudo yum install docker -y
 sudo service docker start
 
-sudo useradd dockeradmin && sudo passwd dockeradmin
+sudo useradd dockeradmin && sudo echo "dockeradmin:dockeradmin" | sudo chpasswd
 sudo usermod -aG docker dockeradmin
